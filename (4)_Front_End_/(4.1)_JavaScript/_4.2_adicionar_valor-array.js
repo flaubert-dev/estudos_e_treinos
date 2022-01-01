f4ab13c9d2db90d161f6c059b0176253d574835d
FHFlaubert <flaubertcontato@gmail.com>
2022-01-01 19:45:19 -0300

    _task-js Adiciona adicionar valor array

---
+++ (4)_Front_End_/(4.1)_JavaScript/_4.2_adicionar_valor-array.js
@@ -0,0 +1,6 @@
+/* inserir valor em um array */
+var numeros = [ 1, 2, 3, 4, 5 ];
+
+numeros[5] = 777; // adiciona um novo indice no array com o valor de 777.
+
+numeros[5] // Antes não existia o índice 5, agora existe. Será exibido 777.
