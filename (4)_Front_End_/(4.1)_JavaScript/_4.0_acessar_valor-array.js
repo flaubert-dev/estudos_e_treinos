0bc662b87089a7be5f3568a5679ad677597cd8d3
FHFlaubert <flaubertcontato@gmail.com>
2022-01-01 19:02:53 -0300

    _task-js Adiciona acessar valor array

---
+++ (4)_Front_End_/(4.1)_JavaScript/_4.0_acessar_valor-array.js
@@ -0,0 +1,41 @@
+/* (1) acessando o valor do array da variável numeros pelo índice */
+var numeros = [ 1, 2, 3, 4, 5 ];
+
+numeros[0] // exibirá 1. O índice (base) inicial em um array é 0
+numeros[1] // exibirá 2.
+numeros[2] // exibirá 3.
+numeros[3] // exibirá 4.
+numeros[4] // exibirá 5.
+/*
+ * Obs: poderá utilizar o console.log() para ver o resultado no navegador
+ * Exemplo: console.log(numeros[0]);
+ * Obs: poderá também apertar F12 ir em console e digitar numeros[0] para ver o resultado no navegador 
+ */
+
+/* (2) acessando o valor do array da variável numeros pelo índice */
+let numeros = [ 'um', 'dois', 'tres', 'quatro', 'cinco' ];
+
+numeros[0] // exibirá um. O índice (base) inicial em um array é 0
+numeros[1] // exibirá dois.
+numeros[2] // exibirá tres.
+numeros[3] // exibirá quatro.
+numeros[4] // exibirá cinco.
+/*
+ * Obs: poderá utilizar o console.log() para ver o resultado no navegador
+ * Exemplo: console.log(numeros[0]);
+ * Obs: poderá também apertar F12 ir em console e digitar numeros[0] para ver o resultado no navegador 
+ */
+
+/* (3) acessando o valor do array da variável numeros pelo índice */
+const numeros = [ 'I', 'II', 'III', 'IV', 'V' ];
+
+numeros[0] // exibirá I. O índice (base) inicial em um array é 0
+numeros[1] // exibirá II.
+numeros[2] // exibirá III.
+numeros[3] // exibirá IV.
+numeros[4] // exibirá V.
+/*
+ * Obs: poderá utilizar o console.log() para ver o resultado no navegador
+ * Exemplo: console.log(numeros[0]);
+ * Obs: poderá também apertar F12 ir em console e digitar numeros[0] para ver o resultado no navegador
+ */
