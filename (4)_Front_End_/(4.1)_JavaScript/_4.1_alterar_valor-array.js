683c703e15d6eb011d95e67c6bca81a6f9be3661
FHFlaubert <flaubertcontato@gmail.com>
2022-01-01 19:36:49 -0300

    _task-js Adiciona alterar valor array

---
+++ (4)_Front_End_/(4.1)_JavaScript/_4.1_alterar_valor-array.js
@@ -0,0 +1,14 @@
+/* alterando valores do array */
+var numeros = [ 1, 2, 3, 4, 5 ];
+
+numeros[0] = 1000; // altera o valor 1 para 1000
+numeros[1] = 2000; // altera o valor 2 para 2000
+numeros[2] = 3000; // altera o valor 3 para 3000
+numeros[3] = 4000; // altera o valor 4 para 4000
+numeros[4] = 5000; // altera o valor 5 para 5000
+
+numeros[0] // agora exibirá 1000. Lembrando, o índice (base) inicial em um array é 0
+numeros[1] // agora exibirá 2000.
+numeros[2] // agora exibirá 3000.
+numeros[3] // agora exibirá 4000.
+numeros[4] // agora exibirá 5000.
