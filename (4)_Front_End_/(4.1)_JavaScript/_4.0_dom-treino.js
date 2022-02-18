45406fe61b2c9055207e374f2df2dd62e98da19a
FHFlaubert <flaubertcontato@gmail.com>
2022-02-18 14:22:41 -0300

    Adiciona estudo pratico DOM

---
+++ (4)_Front_End_/(4.1)_JavaScript/_4.0_dom-treino.js
@@ -0,0 +1,6 @@
+console.log(window);
+// window é o objeto global do browser (navegador)
+// window possui diferentes métodos e propriedades
+
+window.innerHeight;
+// esse exemplo retorna a altura (height) do browser (navegador)
