870d55706f7d3668887ac0d2ea51ecbe3780d480
FHFlaubert <flaubertcontato@gmail.com>
2022-02-18 15:06:58 -0300

    _task-js Estudo DOM (2)

--- (4)_Front_End_/(4.1)_JavaScript/_4.0_dom-treino.js
+++ (4)_Front_End_/(4.1)_JavaScript/_4.0_dom-treino.js
@@ -5,4 +5,13 @@ console.log(window);
 window.innerHeight;
 // esse exemplo retorna a altura (height) do browser (navegador)
 // .innerHeight acessando uma propriedade do objeto window
-// propriedades não executam, só retornam um valor 
+// propriedades não executam, só retornam um valor
+
+window.alert('Isso irá fazer com que apareça um alerta')
+alert('Isso irá fazer com que apareça um alerta sem o objeto window')
+// O window não é obrigatório ser colocado antes do alert() por ser global, 
+// o alert() ou qualquer outro método do objeto window vai funcionar normalmente.
+
+document.querySelector('h1');
+// O objeto document acessa o método querySelector que recebe um argumento 'h1',
+// isso faz com que a primeira tag / elemento h1 dentro do documento (html) seja selecionado.
