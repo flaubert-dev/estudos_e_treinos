8fd46689346b06bc9838e92f9af715216a1f4591
FHFlaubert <flaubertcontato@gmail.com>
2022-02-23 14:09:30 -0300

    _task-js classList (treino) 2

--- (4)_Front_End_/(4.1)_JavaScript_BASE/_4.6_classList-treino.js
+++ (4)_Front_End_/(4.1)_JavaScript_BASE/_4.6_classList-treino.js
@@ -4,3 +4,12 @@ const menu = document.querySelector('.menu');
 menu.className; // Retorna uma string
 menu.classList; // Se tiver o forEach() o classList retorna a lista de classes
 menu.classList.add('ativo'); // Adiciona uma nova classe para o elemento
+menu.classList.add('ativo', 'mobile'); // Adiciona duas classes para o elemento
+menu.classList.remove('ativo'); // Remove a existente no elemento
+menu.classList.toggle('ativo'); // Adiciona e remove a classe no elemento
+
+menu.classList.contains('ativo'); 
+// Confere se o elemento possui a classe CSS: ativo (true ou false)
+
+menu.classList.replace('ativo', 'inativo');
+// Substitui a classe CSS: ativo (classe que existe no elemento) para a classe: inativo
