b3a4d7ca44cbd99d3473d5eb9d61a1cbb1994e70
FHFlaubert <flaubertcontato@gmail.com>
2022-02-23 10:17:23 -0300

    _task-js forEach & Arrow Function (treino)

--- (4)_Front_End_/(4.1)_JavaScript_BASE/_4.5_ES6_forEach_Arrow_Function-treino.js
+++ (4)_Front_End_/(4.1)_JavaScript_BASE/_4.5_ES6_forEach_Arrow_Function-treino.js
@@ -10,3 +10,19 @@ imgs.forEach(function(item, index, array){
 // 2: o método forEach() possui o parâmetro function(), o famoso callback.
 // 3: dentro do parâmetro function() temos os parâmetros: 
 // 4: item(valorAtual), index(numeroIndex), array(arrayCompleta)
+
+// Transformando um **array-like** em **array**: \/ (código abaixo) \/
+
+const titulos = document.getElementsByClassName('titulo');
+// Seleciona todos os elementos que tem a classe CSS: titulo
+const titulosArray = Array.from(titulos);
+// Transforma titulos em um array (verdadeiro)
+
+titulosArray.forEach(function(item){
+  console.log(item);
+});
+// 1: pega os elementos que tem a classe CSS: titulo já transformados em array.
+// 2: acessa (.) o método forEach()
+// 3: o método forEach() possui o parâmetro function(), o famoso callback.
+// 4: dentro do parâmetro function() tem o parâmetro: item
+// 5: cada item (ou elemento) selecionado será mostrado no console do navegador.
