3d473cfcaf4637428d2d2f24183694042c4b04e6
FHFlaubert <flaubertcontato@gmail.com>
2022-02-23 13:02:06 -0300

    _task-js classList (treino)

---
+++ (4)_Front_End_/(4.1)_JavaScript_BASE/_4.6_classList-treino.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu');
+// Seleciona o PRIMEIRO elemento que contém a classe CSS: menu
+
+menu.className; // Retorna uma string
+menu.classList; // Se tiver o forEach() o classList retorna a lista de classes
+menu.classList.add('ativo'); // Adiciona uma nova classe para o elemento
