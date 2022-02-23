b50ff08107badd4f36e2d33f219f0072adbab59e
FHFlaubert <flaubertcontato@gmail.com>
2022-02-23 15:08:43 -0300

    _task-js attributes, getAttribute e setAttribute (teorico)

--- (4)_Front_End_/(4.1)_JavaScript_BASE/_4.6_classList-treino.js
+++ (4)_Front_End_/(4.1)_JavaScript_BASE/_4.6_classList-treino.js
@@ -1,12 +1,16 @@
 const menu = document.querySelector('.menu');
 // Seleciona o PRIMEIRO elemento que contém a classe CSS: menu
 
-menu.className; // Retorna uma string
+menu.className; // Retorna uma string com todas as classes existentes no elemento
 menu.classList; // Se tiver o forEach() o classList retorna a lista de classes
 menu.classList.add('ativo'); // Adiciona uma nova classe para o elemento
 menu.classList.add('ativo', 'mobile'); // Adiciona duas classes para o elemento
 menu.classList.remove('ativo'); // Remove a existente no elemento
-menu.classList.toggle('ativo'); // Adiciona e remove a classe no elemento
+
+menu.classList.toggle('ativo'); 
+// Adiciona e remove a classe no elemento
+// Se tem a classe CSS: ativo o toggle() remove a classe: ativo do elemento
+// Se não tem a classe CSS: ativo o toggle() vai adicionar a classe: ativo no elemento
 
 menu.classList.contains('ativo'); 
 // Confere se o elemento possui a classe CSS: ativo (true ou false)
