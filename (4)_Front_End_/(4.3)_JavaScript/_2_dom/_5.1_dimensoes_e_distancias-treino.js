a0b0c19dd8b7cd352189a8a7d4819dfaf2b00135
FHFlaubert <flaubertcontato@gmail.com>
2022-02-24 10:21:35 -0300

    _task-js Dimensoes e distancias (treino) 2

--- (4)_Front_End_/(4.3)_JavaScript/_2_dom/_5.1_dimensoes_e_distancias-treino.js
+++ (4)_Front_End_/(4.3)_JavaScript/_2_dom/_5.1_dimensoes_e_distancias-treino.js
@@ -1,8 +1,26 @@
+// LEMBRANDO: todo o conceito abaixo vale também para o Width: clientWidth ...
+
 const lista = document.querySelector('.lista');
-// Seleciona o PRIMEIRO elemento com a classe CSS: lista dentro do documento html.
+// Seleciona o PRIMEIRO elemento com a classe CSS: lista dentro do documento html
 
 const listaHeight = lista.clientHeight;
 // Retorna um número informando o tamanho (height) da lista
+// Lembrando que essa propriedade (clientHeight) identifica 
+// se o elemento tem um padding que aumente o tamanho (height), mas não um border (por exemplo),
+// para identificar: height + paddding + border, teria que utilizar a propriedade:
+// offsetHeight 
 
 console.log(listaHeight);
 // Mostra no console do navegador o tamanho (height) da lista
+
+// \/\/ No exemplo abaixo se a lista tiver um overflow-y: scroll (Propriedade CSS)
+// \/\/ Será retornado o tamanho (height) do scroll
+
+const listaComScroll = document.querySelector('.lista-scroll');
+// Seleciona o PRIMEIRO elemento com a classe CSS: lista-scroll dentro do documento html
+
+const listaComScrollHeight = listaComScroll.scrollHeight;
+// Retorna um número informando o tamanho total (height) do scroll da lista
+
+console.log(listaComScrollHeight);
+// Mostra no console do navegador o tamanho total (height) do scroll da lista
