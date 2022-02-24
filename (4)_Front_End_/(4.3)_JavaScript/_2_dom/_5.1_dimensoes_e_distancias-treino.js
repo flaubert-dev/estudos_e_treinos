ed6c059873ec2d2ffd09872cb8bd27fb2fa05e91
FHFlaubert <flaubertcontato@gmail.com>
2022-02-24 09:57:25 -0300

    _task-js Dimensoes e distancias (treino)

---
+++ (4)_Front_End_/(4.3)_JavaScript/_2_dom/_5.1_dimensoes_e_distancias-treino.js
@@ -0,0 +1,8 @@
+const lista = document.querySelector('.lista');
+// Seleciona o PRIMEIRO elemento com a classe CSS: lista dentro do documento html.
+
+const listaHeight = lista.clientHeight;
+// Retorna um número informando o tamanho (height) da lista
+
+console.log(listaHeight);
+// Mostra no console do navegador o tamanho (height) da lista
