5ddeed5eab4857ece3dfad24f69ad3634a819756
Flaubert Henriques <53451516+Dev-Flaubert-Wordpress@users.noreply.github.com>
2022-12-25 14:08:24 -0300

    _main Atualiza _0_insights_gerais.js

--- (3)_Front_End_/(3.3)_JavaScript/_0_insights_gerais/_0_insights_gerais.js
+++ (3)_Front_End_/(3.3)_JavaScript/_0_insights_gerais/_0_insights_gerais.js
@@ -1,80 +1,56 @@
-// Anotações aleatórias (insights, sacadas) que eu tive durante o meu aprendizado.
-// Atenção, ALGUMAS partes podem ser super avançadas para alguns ou poderá não compreender
-// o que estou querendo dizer...
+// Anotações básicas (insights) que documentei durante o meu aprendizado >> inicial << em JS.
+// Atenção: ALGUMAS partes podem ser avançadas para alguns ou poderá ser difícil de compreender o que estou querendo dizer...
 
 /* Insight (1)
- * Indices são gerados AUTOMATICAMENTE em um Array [0] [1] [2] [3]
- * Indices Criados em um Array ['exemploUm'] ['exemploDois'] ['exemploTres']
+ * Indices (index): são gerados AUTOMATICAMENTE em um Array [0] [1] [2] [3]
  */
 
 /* Insight (2)
- * var amount      = document.getElementById( "amount" );
- * var principal   = parseFloat( amount.value );
- * (1) var amount (variavel)
- * (2) document (olha o documento)
- * (3) getElementById (pega o elemento (marcação html) que tem o ID amount)
- * (4) var principal (variavel)
- * (5) parseFloat (função, método)
- * (6) amount.value (variavel amount acessa propriedade value)
- * Se amount acessa uma propriedade é por que amount também é um objeto. 
- * Ou seja, tudo em JavaScript é Objeto! \o/
- */
-
-/* Insight (3)
  * VSCode (IDE) .Propriedade (barra de ouro AZUL)
  * VSCode (IDE) .Metodo() (cubo magico ROXO)
  */
 
-/* Insight (4)
- * Math (Objeto nativo do JS). Colocando o objeto e apertando o enter no console
- * você tem acesso as Propriedades e Metodos.
- * Math.PI (Objeto acessando uma Propriedade [obs: variavel]).
+/* Insight (3)
+ * Math (Objeto nativo do JS). Colocando o objeto e apertando o enter no console do browser (navegador) você tem acesso as Propriedades e aos Metodos.
+ * Math.PI (Objeto acessando uma Propriedade, obs: variavel).
  * Math.abs(5) (Objeto acessando um Método e colocando um valor para retornar).
  * console.log(Math.abs(5)); (Retorna o valor 5 no console do navegador).
  */
 
+/* Insight (4)
+ * console (Objeto nativo do JS). Colocando o objeto e apertando o enter no console você tem acesso aos Metodos.
+ * console.log() (Objeto acessando um Método, obs: função).
+ */
+
 /* Insight (5)
- * console (Objeto nativo do JS). Colocando o objeto e apertando o enter no console
- * você tem acesso aos Metodos.
- * console.log() (Objeto acessando um Método [obs: função]).
- * console.log(Math.abs(5)) (Objeto acessando um Método e colocando um valor para retornar).
+ * GET (acessa, pega, puxa um valor para mostrar no "front-end") objeto.propriedade || objeto.metodo()
+ * SET (atribui, adiciona, insere, injeta um novo valor no "back-end") objeto.propriedade = "novo valor";
+ * this (acessa, pega, puxa tudo que tem dentro do próprio Objeto) this.propriedade || this.metodo()
  */
 
 /* Insight (6)
- * GET (acessa, pega, puxa um valor para mostrar no "front-end") objeto.propriedade || objeto.metodo()
+ * Propriedade (variavel dentro do Objeto)
+ * Método (função dentro do Objeto)
+ * Objeto (possui Propriedades e Métodos dentro dele)
+ * Para facilitar: tudo em JavaScript é objeto. Na grande maioria dos casos, vamos ter que acessar as propriedades e métodos de um objeto JS nativo.
  */
 
 /* Insight (7)
- * this (acessa, pega, puxa tudo que tem dentro do Objeto) this.propriedade || this.metodo() 
- */
-
-/* Insight (8)
- * SET (atribui, adiciona, insere, injeta um novo valor no "back-end") objeto.propriedade = "novo valor"; 
- */
-
-/* Insight (9)
- * Propriedade (variavel dentro do Objeto)
- * Método (função dentro do Objeto)
- * Objeto (possui Propriedades e Métodos dentro dele) 
- */
-
-/* Insight (10)
- * Função (sempre retorna um valor, para executar uma função precisa do () parenteses)
- * Função (se a função não retornar nada colocando o parenteses, 
- * é necessário colocar, inserir, adicionar um valor dentro do parenteses)
- */
-
-/* Insight (11)
  * Na verdade, saber / entender bem as Propriedades e Métodos dos diferentes Objetos do JS
  * é que é possível compreender o JS.
  */
 
-/* Insight (12)
- * Quando uma função recebe um parametro, exemplo: function mulher(parametro){ return parametro }
- * é porque precisa receber uma entrada (input), exemplo: mulher("I love you")
- * console.log(mulher("I love you")); // Function Recebe o valor "I love you" e retorna o parametro com o valor "I love you" no console do browser. \o/
+/* Insight (8)
+ * Função (sempre retorna um valor, para executar uma função precisa do () parenteses)
  */
 
-/* Insight (13)
- * No function a entrada (input) está nos parênteses () e a saída está no return
+/* Insight (9)
+ * Quando uma função recebe um parametro, exemplo: function mulher(parametro){ return parametro }
+ * é porque precisa receber um argumento (input, entrada), exemplo: mulher("I love you")
+ * console.log(mulher("I love you")); // Função ativada que recebe o argumento: "I love you" e 
+ * retorna a frase: I love you no console do browser (navegador). :)
+ */
+
+/* Insight (10)
+ * No function o argumento (input, entrada) está nos parênteses () e a saída está no return
  */
