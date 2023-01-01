0d9a4073607bd2967e0ae234fecfd8be86cb069e
Flaubert Henriques <53451516+flaubert-dev@users.noreply.github.com>
2023-01-01 16:47:40 -0300

    _main Atualiza _0_insights_gerais.js

--- (3)_Front_End_/(3.3)_JavaScript/_0_insights_gerais/_0_insights_gerais.js
+++ (3)_Front_End_/(3.3)_JavaScript/_0_insights_gerais/_0_insights_gerais.js
@@ -1,43 +1,43 @@
 // Anotações básicas (insights) que documentei durante o meu aprendizado >> inicial << em JS.
-// Atenção: ALGUMAS partes podem ser avançadas para alguns ou poderá ser difícil de compreender o que estou querendo dizer...
+// Atenção: ALGUNS novos insights podem ser avançados ou difícil de compreender o que estou querendo dizer...
 
 /* Insight (1)
  * Indices (index): são gerados AUTOMATICAMENTE em um Array [0] [1] [2] [3]
  */
 
 /* Insight (2)
- * VSCode (IDE) .Propriedade (barra de ouro AZUL)
- * VSCode (IDE) .Metodo() (cubo magico ROXO)
- */
-
-/* Insight (3)
- * Math (Objeto nativo do JS). Colocando o objeto e apertando o enter no console do browser (navegador) você tem acesso as Propriedades e aos Metodos.
- * Math.PI (Objeto acessando uma Propriedade, obs: variavel).
+ * Math (Objeto nativo do JS). Colocando o objeto e apertando o enter no console do browser (navegador) você tem acesso aos Atributos (propriedades) e aos Metodos.
+ * Math.PI (Objeto acessando um atributo (propriedade), obs: variavel).
  * Math.abs(5) (Objeto acessando um Método e colocando um valor para retornar).
  * console.log(Math.abs(5)); (Retorna o valor 5 no console do navegador).
  */
 
+/* Insight (3)
+ * VSCode (IDE) .Atributo ou Propriedade (barra AZUL)
+ * VSCode (IDE) .Metodo() (cubo magico ROXO)
+ */
+
 /* Insight (4)
+ * Atributo ou propriedade: variavel dentro do Objeto
+ * Método: função dentro do Objeto
+ * Objeto: possui Atributos (propriedades) e Métodos dentro dele.
+ * Para facilitar: tudo em JavaScript é objeto. Na grande maioria dos casos, vamos ter que acessar os atributos e métodos de um objeto JS nativo.
+ */
+
+/* Insight (5)
+ * Na verdade, saber / entender bem os Atributos (propriedades) e Métodos dos diferentes Objetos do JS
+ * é que é possível compreender o JS.
+ */
+
+/* Insight (6)
  * console (Objeto nativo do JS). Colocando o objeto e apertando o enter no console você tem acesso aos Metodos.
  * console.log() (Objeto acessando um Método, obs: função).
  */
 
-/* Insight (5)
- * GET (acessa, pega, puxa um valor para mostrar no "front-end") objeto.propriedade || objeto.metodo()
- * SET (atribui, adiciona, insere, injeta um novo valor no "back-end") objeto.propriedade = "novo valor";
- * this (acessa, pega, puxa tudo que tem dentro do próprio Objeto) this.propriedade || this.metodo()
- */
-
-/* Insight (6)
- * Propriedade (variavel dentro do Objeto)
- * Método (função dentro do Objeto)
- * Objeto (possui Propriedades e Métodos dentro dele)
- * Para facilitar: tudo em JavaScript é objeto. Na grande maioria dos casos, vamos ter que acessar as propriedades e métodos de um objeto JS nativo.
- */
-
 /* Insight (7)
- * Na verdade, saber / entender bem as Propriedades e Métodos dos diferentes Objetos do JS
- * é que é possível compreender o JS.
+ * GET (acessa, pega, puxa um valor) objeto.atributo || objeto.metodo()
+ * SET (altera, atribui, adiciona, insere, injeta um novo valor) objeto.atributo = "novo valor";
+ * this (acessa, pega, puxa tudo que tem dentro do próprio Objeto) this.atributo || this.metodo()
  */
 
 /* Insight (8)
@@ -52,5 +52,5 @@
  */
 
 /* Insight (10)
- * No function o argumento (input, entrada) está nos parênteses () e a saída está no return
+ * No function o argumento (input, entrada) está nos parênteses () e a saída está no return + ativação da função
  */
